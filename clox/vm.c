e2892f777644ae86cd6189b016f5eaa28404ff54
harmeets43 <harmeets43@gmail.com>
2020-04-28 22:20:03 -0500

    Finished the constant stack implementation

--- clox/vm.c
+++ clox/vm.c
@@ -5,7 +5,12 @@
 
 VM vm;
 
+static void resetStack() {
+	vm.stackTop = vm.stack;
+}
+
 void initVM() {
+	resetStack();
 }
 
 void freeVM() {
@@ -19,7 +24,15 @@ static InterpretResult run() {
 
 	for (;;) {
 
-#ifdef DEBUG_TRACE_EXECUTION                                        
+#ifdef DEBUG_TRACE_EXECUTION
+		printf("          ");
+		for (Value* slot = vm.stack; slot < vm.stackTop; slot++) {
+			printf("[ ");
+			printValue(*slot);
+			printf(" ]");
+		}
+		printf("\n");
+
 		disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
 #endif 
 
@@ -28,11 +41,14 @@ static InterpretResult run() {
 		switch (instruction = READ_BYTE()) {
 			case OP_CONSTANT: {
 				Value constant = READ_CONSTANT();
+				push(constant);
 				printValue(constant);
 				printf("\n");
 				break;
 			}
 			case OP_RETURN: {
+				printValue(pop());
+				printf("\n");
 				return INTERPRET_OK;
 			}
 		}
@@ -46,4 +62,14 @@ InterpretResult interpret(Chunk* chunk) {
 	vm.chunk = chunk;
 	vm.ip = vm.chunk->code;
 	return run();
+}
+
+void push(Value value) {
+	*vm.stackTop = value;
+	vm.stackTop++;
+}
+
+Value pop() {
+	vm.stackTop--;
+	return *vm.stackTop;
 }
