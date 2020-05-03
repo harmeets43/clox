e6687cd9eb69be15170e6b893bacba1fe2e0600e
harmeets43 <harmeets43@gmail.com>
2020-05-03 13:59:03 -0500

    Adding some more features to the language implementation like equal greater and less than operator

--- clox/compiler.c
+++ clox/compiler.c
@@ -68,7 +68,7 @@ static void errorAt(Token* token, const char* message) {
 }
 
 static void error(const char* message) {
-	
+
 	errorAt(&parser.previous, message);
 }
 
@@ -140,7 +140,7 @@ static void parsePrecedence(Precedence precedence);
 
 
 /*
-	When a prefix parser function is called, the leading token has already been consumed. 
+	When a prefix parser function is called, the leading token has already been consumed.
 	An infix parser function is even more in medias res—the left-hand operand has already been compiled and the subsequent infix operator consumed.
 	The fact that the left operand gets compiled first works out fine. It means at runtime, that code gets executed first.
 	When it runs, the value it produces will end up on the stack. That’s right where the infix operator is going to need it.
@@ -156,12 +156,28 @@ static void binary() {
 
 	// Emit the operator instruction.                        
 	switch (operatorType) {
+	case TOKEN_BANG_EQUAL:    emitBytes(OP_EQUAL, OP_NOT); break;
+	case TOKEN_EQUAL_EQUAL:   emitByte(OP_EQUAL); break;
+	case TOKEN_GREATER:       emitByte(OP_GREATER); break;
+	case TOKEN_GREATER_EQUAL: emitBytes(OP_LESS, OP_NOT); break;
+	case TOKEN_LESS:          emitByte(OP_LESS); break;
+	case TOKEN_LESS_EQUAL:    emitBytes(OP_GREATER, OP_NOT); break;
 	case TOKEN_PLUS:          emitByte(OP_ADD); break;
 	case TOKEN_MINUS:         emitByte(OP_SUBTRACT); break;
 	case TOKEN_STAR:          emitByte(OP_MULTIPLY); break;
 	case TOKEN_SLASH:         emitByte(OP_DIVIDE); break;
 	default:
 		return; // Unreachable.                              
+	}
+}
+
+static void literal() {
+	switch (parser.previous.type) {
+	case TOKEN_FALSE: emitByte(OP_FALSE); break;
+	case TOKEN_NIL: emitByte(OP_NIL); break;
+	case TOKEN_TRUE: emitByte(OP_TRUE); break;
+	default:
+		return; // Unreachable.                   
 	}
 }
 
@@ -184,6 +200,7 @@ static void unary() {
 
 	// Emit the operator instruction.              
 	switch (operatorType) {
+	case TOKEN_BANG: emitByte(OP_NOT); break;
 	case TOKEN_MINUS: emitByte(OP_NEGATE); break;
 	default:
 		return; // Unreachable.                    
@@ -202,31 +219,31 @@ ParseRule rules[] = {
   { NULL,     NULL,    PREC_NONE },       // TOKEN_SEMICOLON       
   { NULL,     binary,  PREC_FACTOR },     // TOKEN_SLASH           
   { NULL,     binary,  PREC_FACTOR },     // TOKEN_STAR            
-  { NULL,     NULL,    PREC_NONE },       // TOKEN_BANG            
-  { NULL,     NULL,    PREC_NONE },       // TOKEN_BANG_EQUAL      
+  { unary,     NULL,    PREC_NONE },       // TOKEN_BANG            
+  { NULL,     binary,   PREC_EQUALITY },       // TOKEN_BANG_EQUAL      
   { NULL,     NULL,    PREC_NONE },       // TOKEN_EQUAL           
-  { NULL,     NULL,    PREC_NONE },       // TOKEN_EQUAL_EQUAL     
-  { NULL,     NULL,    PREC_NONE },       // TOKEN_GREATER         
-  { NULL,     NULL,    PREC_NONE },       // TOKEN_GREATER_EQUAL   
-  { NULL,     NULL,    PREC_NONE },       // TOKEN_LESS            
-  { NULL,     NULL,    PREC_NONE },       // TOKEN_LESS_EQUAL      
+   { NULL,     binary,  PREC_EQUALITY },   // TOKEN_EQUAL_EQUAL  
+  { NULL,     binary,  PREC_COMPARISON }, // TOKEN_GREATER      
+  { NULL,     binary,  PREC_COMPARISON }, // TOKEN_GREATER_EQUAL
+  { NULL,     binary,  PREC_COMPARISON }, // TOKEN_LESS         
+  { NULL,     binary,  PREC_COMPARISON }, // TOKEN_LESS_EQUAL      
   { NULL,     NULL,    PREC_NONE },       // TOKEN_IDENTIFIER      
   { NULL,     NULL,    PREC_NONE },       // TOKEN_STRING          
   { number,   NULL,    PREC_NONE },       // TOKEN_NUMBER          
   { NULL,     NULL,    PREC_NONE },       // TOKEN_AND             
   { NULL,     NULL,    PREC_NONE },       // TOKEN_CLASS           
   { NULL,     NULL,    PREC_NONE },       // TOKEN_ELSE            
-  { NULL,     NULL,    PREC_NONE },       // TOKEN_FALSE           
+  { literal,     NULL,    PREC_NONE },       // TOKEN_FALSE           
   { NULL,     NULL,    PREC_NONE },       // TOKEN_FOR             
   { NULL,     NULL,    PREC_NONE },       // TOKEN_FUN             
   { NULL,     NULL,    PREC_NONE },       // TOKEN_IF              
-  { NULL,     NULL,    PREC_NONE },       // TOKEN_NIL             
+  { literal,     NULL,    PREC_NONE },       // TOKEN_NIL             
   { NULL,     NULL,    PREC_NONE },       // TOKEN_OR              
   { NULL,     NULL,    PREC_NONE },       // TOKEN_PRINT           
   { NULL,     NULL,    PREC_NONE },       // TOKEN_RETURN          
   { NULL,     NULL,    PREC_NONE },       // TOKEN_SUPER           
   { NULL,     NULL,    PREC_NONE },       // TOKEN_THIS            
-  { NULL,     NULL,    PREC_NONE },       // TOKEN_TRUE            
+  { literal,     NULL,    PREC_NONE },       // TOKEN_TRUE            
   { NULL,     NULL,    PREC_NONE },       // TOKEN_VAR             
   { NULL,     NULL,    PREC_NONE },       // TOKEN_WHILE           
   { NULL,     NULL,    PREC_NONE },       // TOKEN_ERROR           
