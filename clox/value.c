4dfdb5637cd16ec7e5d332ec3778ecf56ca239c3
harmeets43 <harmeets43@gmail.com>
2020-05-03 14:34:54 -0500

    Added support for string using making a new object type

--- clox/value.c
+++ clox/value.c
@@ -1,5 +1,6 @@
 #include <stdio.h>                      
-
+#include <string.h>
+#include "object.h"
 #include "memory.h"                     
 #include "value.h"                      
 
@@ -41,5 +42,10 @@ bool valuesEqual(Value a, Value b) {
 	case VAL_BOOL:   return AS_BOOL(a) == AS_BOOL(b);
 	case VAL_NIL:    return true;
 	case VAL_NUMBER: return AS_NUMBER(a) == AS_NUMBER(b);
+		case VAL_OBJ: {
+			ObjString* aString = AS_STRING(a);
+			ObjString* bString = AS_STRING(b);
+			return aString->length == bString->length && memcmp(aString->chars, bString->chars, aString->length) == 0;
+		}
 	}
 }
