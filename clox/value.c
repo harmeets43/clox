16348616abf5dfc5e87b1c1d9d29c65efbcff4cb
harmeets43 <harmeets43@gmail.com>
2020-05-03 15:22:22 -0500

    Adding cleaning code to  cleanup the intermedaite strings

--- clox/value.c
+++ clox/value.c
@@ -32,6 +32,7 @@ void printValue(Value value) {
 	case VAL_BOOL:   printf(AS_BOOL(value) ? "true" : "false"); break;
 	case VAL_NIL:    printf("nil"); break;
 	case VAL_NUMBER: printf("%g", AS_NUMBER(value)); break;
+	case VAL_OBJ: printObject(value); break;
 	}
 }
 
