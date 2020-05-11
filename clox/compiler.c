19522c4774af40b0d980151a2b82b55383444e4f
harmeets43 <harmeets43@gmail.com>
2020-05-11 14:31:55 -0500

    Finished the while loop support

--- clox/compiler.c
+++ clox/compiler.c
@@ -130,6 +130,18 @@ static void emitBytes(uint8_t byte1, uint8_t byte2) {
 	emitByte(byte2);
 }
 
+static void emitLoop(int loopStart) {
+
+	emitByte(OP_LOOP);
+
+	int offset = currentChunk()->count - loopStart + 2;
+	if (offset > UINT16_MAX) error("Loop body too large.");
+
+	emitByte((offset >> 8) & 0xff);
+	emitByte(offset & 0xff);
+
+}
+
 static int emitJump(uint8_t instruction) {
 	emitByte(instruction);
 	emitByte(0xff);
@@ -523,6 +535,24 @@ static void ifStatement() {
 	patchJump(elseJump);
 }
 
+static void whileStatement() {
+	int loopStart = currentChunk()->count;
+
+	consume(TOKEN_LEFT_PAREN, "Expect '(' after 'while'.");
+	expression();
+	consume(TOKEN_RIGHT_PAREN, "Expect ')' after condition.");
+
+	int exitJump = emitJump(OP_JUMP_IF_FALSE);
+
+	emitByte(OP_POP);
+	statement();
+
+	emitLoop(loopStart);
+
+	patchJump(exitJump);
+	emitByte(OP_POP);
+}
+
 static void printStatement() {
 	expression();
 	consume(TOKEN_SEMICOLON, "Expect ';' after value");
@@ -563,6 +593,9 @@ static void statement() {
 	}
 	else if (match(TOKEN_IF)) {
 		ifStatement();
+	}
+	else if (match(TOKEN_WHILE)) {
+		whileStatement();
 	}
 	else if (match(TOKEN_LEFT_BRACE)) {
 		beginScope();
--- clox/vm.c
+++ clox/vm.c
@@ -209,6 +209,12 @@ static InterpretResult run() {
 			if (isFalsey(peek(0))) vm.ip += offset;
 			break;
 		}
+		case OP_LOOP: {
+			// go back to offset in the chunk array.
+			uint16_t offset = READ_SHORT();
+			vm.ip -= offset;
+			break;
+		}
 		case OP_RETURN: {
 			//Exit interpreter.
 			return INTERPRET_OK;
