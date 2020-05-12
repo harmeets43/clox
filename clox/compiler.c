31a568192b95573c1a7e5039fecc4f82d1c310bb
harmeets43 <harmeets43@gmail.com>
2020-05-11 21:21:06 -0500

    Finished classes and instances support

--- clox/compiler.c
+++ clox/compiler.c
@@ -496,13 +496,26 @@ static void or_(bool canAssign) {
 	patchJump(endJump);
 }
 
+static void dot(bool canAssign) {
+	consume(TOKEN_IDENTIFIER, "Expect property name after '.'.");
+	uint8_t name = identifierConstant(&parser.previous);
+
+	if (canAssign && match(TOKEN_EQUAL)) {
+		expression();
+		emitBytes(OP_SET_PROPERTY, name);
+	}
+	else {
+		emitBytes(OP_GET_PROPERTY, name);
+	}
+}
+
 ParseRule rules[] = {
   { grouping, call,    PREC_CALL },       // TOKEN_LEFT_PAREN      
   { NULL,     NULL,    PREC_NONE },       // TOKEN_RIGHT_PAREN     
   { NULL,     NULL,    PREC_NONE },      // TOKEN_LEFT_BRACE
   { NULL,     NULL,    PREC_NONE },       // TOKEN_RIGHT_BRACE     
   { NULL,     NULL,    PREC_NONE },       // TOKEN_COMMA           
-  { NULL,     NULL,    PREC_NONE },       // TOKEN_DOT             
+  { NULL,     dot,    PREC_CALL },       // TOKEN_DOT             
   { unary,    binary,  PREC_TERM },       // TOKEN_MINUS           
   { NULL,     binary,  PREC_TERM },       // TOKEN_PLUS            
   { NULL,     NULL,    PREC_NONE },       // TOKEN_SEMICOLON       
@@ -836,10 +849,25 @@ static void statement() {
 	}
 }
 
+static void classDeclaration() {
+	consume(TOKEN_IDENTIFIER, "Expect class name.");
+	uint8_t nameConstant = identifierConstant(&parser.previous);
+	declareVariable();
+
+	emitBytes(OP_CLASS, nameConstant);
+	defineVariable(nameConstant);
+
+	consume(TOKEN_LEFT_BRACE, "Expect '{' before class body.");
+	consume(TOKEN_RIGHT_BRACE, "Expect '}' after class body.");
+}
+
 static void declaration() {
 	
 	// maybe strategy pattern
-	if (match(TOKEN_FUN)) {
+	if (match(TOKEN_CLASS)) {
+		classDeclaration();
+	}
+	else if (match(TOKEN_FUN)) {
 		funDeclaration();
 
 	}
--- clox/memory.c
+++ clox/memory.c
@@ -76,6 +76,11 @@ static void blackenObject(Obj* object) {
 #endif 
 
 	switch (object->type) {
+	case OBJ_CLASS: {
+		ObjClass* klass = (ObjClass*)object;
+		markObject((Obj*)klass->name);
+		break;
+	}
 	case OBJ_CLOSURE: {
 		ObjClosure* closure = (ObjClosure*)object;
 		markObject((Obj*)closure->function);
@@ -88,6 +93,12 @@ static void blackenObject(Obj* object) {
 		ObjFunction* function = (ObjFunction*)object;
 		markObject((Obj*)function->name);
 		markArray(&function->chunk.constants);
+		break;
+	}
+	case OBJ_INSTANCE: {
+		ObjInstance* instance = (ObjInstance*)object;
+		markObject((Obj*)instance->klass);
+		markTable(&instance->fields);
 		break;
 	}
 	case OBJ_UPVALUE:
@@ -105,6 +116,10 @@ static void freeObject(Obj* object) {
 	printf("%p free type %d\n", (void*)object, object->type);
 #endif
 	switch (object->type) {
+	case OBJ_CLASS: {
+		FREE(ObjClass, object);
+		break;
+	}
 	case OBJ_CLOSURE: {
 		ObjClosure* closure = (ObjClosure*)object;
 		FREE_ARRAY(ObjUpvalue*, closure->upvalues, closure->upvalueCount);
@@ -117,7 +132,12 @@ static void freeObject(Obj* object) {
 		FREE(ObjFunction, object);
 		break;
 	}
-
+	case OBJ_INSTANCE: {
+		ObjInstance* instance = (ObjInstance*)object;
+		freeTable(&instance->fields);
+		FREE(ObjInstance, object);
+		break;
+	}
 	case OBJ_NATIVE:
 		FREE(ObjNative, object);
 		break;
